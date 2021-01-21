039433151e5d04a97441f87a325b2515339d7b2a
Emmanuel Egomson <fegomson@gmail.com>
2021-01-21 13:14:03 -0600

    connected to the socket.io client

--- mobile/App.js
+++ mobile/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   SafeAreaView,
   Text,
@@ -7,13 +7,20 @@ import {
 import io from "socket.io-client"
 import tailwind from 'tailwind-rn';
 
-const App = () => (
+const App = () => {
+
+  useEffect(() => {
+    const socket = io('http://127.0.0.1:3000')
+  }, [])  
+
+  return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={tailwind('h-full flex justify-center')}>
         <Text style={tailwind('text-center text-4xl')}>Welcome to TaxiApp</Text>
       </SafeAreaView>
     </>
-);
+  )
+}
 
 export default App;
