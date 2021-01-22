dc0ba330f3ec59b52bab0b2a35fff456f49bd13b
Emmanuel Egomson <fegomson@gmail.com>
2021-01-22 16:58:59 -0600

    implemented the textInput for chatMessages

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   Text,
+  TextInput,
   StatusBar,
+  View,
 } from 'react-native';
 import io from "socket.io-client"
 import tailwind from 'tailwind-rn';
 
-const App = () => {
+class App extends Component {
 
-  useEffect(() => {
-    const socket = io('http://127.0.0.1:3000')
-  }, [])  
+  state = {
+    chatMessage: ""
+  }
 
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={tailwind('h-full flex justify-center')}>
-        <Text style={tailwind('text-center text-4xl')}>Welcome to TaxiApp</Text>
-      </SafeAreaView>
-    </>
-  )
+  componentDidMount() {
+    this.socket = io('http://127.0.0.1:3000')
+  }
+
+  render() {
+    const {chatMessage} = this.state
+
+    const submitChatMessage = () => {
+      this.socket.emit('chat message', this.state.chatMessage)
+      this.setState({
+        chatMessage: ""
+      })
+    }
+
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={tailwind('h-full')}>
+          <TextInput 
+            value={chatMessage}
+            placeholder="Type your message here..."
+            autoCorrect={false} 
+            onChangeText={chatMessage => this.setState({chatMessage})} 
+            onSubmitEditing={() => submitChatMessage()} 
+            style={tailwind('m-4 p-4 rounded-full border-2')}/>
+          <View style={tailwind('p-6')}>
+          </View>
+        </SafeAreaView>
+      </>
+    );
+  }
 }
 
 export default App;
